e6c4f610f2790fb36834c20f0e64374333efe75f
jos3duardo <jos3duardolopes@gmail.com>
2021-10-02 14:34:00 -0400

    feat: criando serviço de criação de products
    Signed-off-by: jos3duardo <jos3duardolopes@gmail.com>

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,29 @@
+import {getCustomRepository} from 'typeorm'
+import ProductsRepository from '@modules/products/typeorm/repositories/ProductsRepository'
+import AppError from '@shared/errors/AppError'
+import Product from '@modules/products/typeorm/entities/Product'
+
+interface IRequest {
+  name: string
+  price: number
+  quantity: number
+}
+
+class CreateProductService {
+  public async execute({name, price, quantity}: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductsRepository)
+    const productExists = await productsRepository.findByName(name)
+    if (productExists) {
+      throw new AppError('There is already one product with this name')
+    }
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    })
+
+    await productsRepository.save(product)
+
+    return product
+  }
+}
