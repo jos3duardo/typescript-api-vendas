0ea14307b1c1e80e9bb0642836b1d5cd975c1cb8
jos3duardo <jos3duardolopes@gmail.com>
2021-10-02 00:01:09 -0400

    feat: padronizando codigo

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,9 @@
-import {Router} from "express";
+import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Dev!'})
+  return res.json({ message: 'Hello Dev!' })
 })
-
 
 export default routes
