59e9a6bd058685ed52a661c41dc7a5fe4203610c
jos3duardo <jos3duardolopes@gmail.com>
2021-10-02 13:32:22 -0400

    env: configs do typeorm
    Signed-off-by: jos3duardo <jos3duardolopes@gmail.com>

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata'
-import express, {NextFunction, Request, Response} from 'express'
+import express, {Request, Response} from 'express'
 import cors from 'cors'
 
 import routes from './routes'
-import AppError from '@shared/errors/AppError'
+import '@shared/typeorm';
+import AppError from '@shared/errors/AppError';
 
 const app = express()
 
@@ -12,22 +13,21 @@ app.use(express.json())
 
 app.use(routes)
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      })
-    }
-
-    return response.status(500).json({
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
       status: 'error',
-      message: 'Internal server error',
+      message: error.message,
     })
-  },
-)
+  }
+  console.log(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(3333, () => {
-  console.log(`Servidor rodando na porta 3333!`)
+  console.log(`Servidor rodando na porta 3333! 🏆`)
 })
