4f1771c0fb62f0d1e95b83e7dffc4864d66cf1bb
jos3duardo <jos3duardolopes@gmail.com>
2021-10-05 01:38:44 -0400

    feat: importando arquivo de rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productsRoutes from '@modules/products/routes/products.routes';
+import usersRoutes from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRoutes);
+routes.use('/users', usersRoutes);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'api online!' });
