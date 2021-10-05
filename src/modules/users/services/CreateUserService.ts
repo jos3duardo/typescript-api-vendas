c168fccd1d3ce1a3403def33ed571738cb6b9b19
jos3duardo <jos3duardolopes@gmail.com>
2021-10-05 01:58:35 -0400

    fix: ajustes no server.ts

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -12,9 +12,11 @@ interface IRequest {
 class CreateUserService {
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
+
     const emailExists = await usersRepository.findByEmail(email);
+
     if (emailExists) {
-      throw new AppError('Email address alread used.');
+      throw new AppError('Email address already used.');
     }
 
     const user = usersRepository.create({
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -14,15 +14,16 @@ app.use(express.json());
 
 app.use(routes);
 app.use(errors());
+
 app.use((error: Error, request: Request, response: Response) => {
+  console.log(error.message);
+
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: 'error',
       message: error.message,
     });
   }
-
-  console.log(error);
 
   return response.status(500).json({
     status: 'error',
