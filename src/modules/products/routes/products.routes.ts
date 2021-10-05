182ac650c7998c026e277fd79165d4bf8ad6d564
jos3duardo <jos3duardolopes@gmail.com>
2021-10-04 23:33:37 -0400

    feat: criando rotas para produtos

---
+++ src/modules/products/routes/products.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import ProductsController from '@modules/products/controllers/ProductsController';
+const productsRoutes = Router();
+const productsController = new ProductsController();
+
+productsRoutes.get('/', productsController.index);
+productsRoutes.get('/:id', productsController.show);
+productsRoutes.post('/', productsController.create);
+productsRoutes.put('/:id', productsController.update);
+productsRoutes.delete('/:id', productsController.delete);
+
+export default productsRoutes;
