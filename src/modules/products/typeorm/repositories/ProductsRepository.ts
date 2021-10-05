d2e1cb9820224469f32a387e16d59c2eb1a8af53
jos3duardo <jos3duardolopes@gmail.com>
2021-10-04 23:33:12 -0400

    fix:  corrigindo decorator no repository de products

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,8 +1,7 @@
-import { EntityRepository, In, Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import Product from '../entities/Product';
 
-EntityRepository(Product);
-
+@EntityRepository(Product)
 class ProductsRepository extends Repository<Product> {
   public async findByName(name: string): Promise<Product | undefined> {
     return await this.findOne({
