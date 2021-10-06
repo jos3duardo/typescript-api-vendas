e5ce6adff455fc138c05483b908be4708f4327bb
jos3duardo <jos3duardolopes@gmail.com>
2021-10-06 00:00:58 -0400

    feat: movendo meddleware de autenticação

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import UserController from '@modules/users/controllers/UserController';
-import isAutenticated from '@modules/users/middlewares/isAuthenticated';
+import isAutenticated from '@shared/http/middlewares/isAuthenticated';
 
 const usersRoutes = Router();
 const usersController = new UserController();
