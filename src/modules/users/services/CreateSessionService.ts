46ed0ec85f8d80af0cd57287f540351ae01d03d4
jos3duardo <jos3duardolopes@gmail.com>
2021-10-05 22:32:01 -0400

    feat: criando controller de session

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,37 @@
+import User from '@modules/users/typeorm/entities/User';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '@modules/users/typeorm/repositories/UsersRepository';
+import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+}
+
+class CreateSessionService {
+  public async execute({ email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      console.log(user);
+      throw new AppError('Incorrect email/password combination.');
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password combination.');
+    }
+
+    return user;
+  }
+}
+
+export default CreateSessionService;
