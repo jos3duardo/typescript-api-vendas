7bfd0d545fd1f252613a36810046470990a467a0
jos3duardo <jos3duardolopes@gmail.com>
2021-10-05 22:32:21 -0400

    feat: criando rotas de session

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import productsRoutes from '@modules/products/routes/products.routes';
 import usersRoutes from '@modules/users/routes/users.routes';
+import sessionRoutes from '@modules/users/routes/session.routes';
 
 const routes = Router();
+
+routes.use('/sessions', sessionRoutes);
 
 routes.use('/products', productsRoutes);
 routes.use('/users', usersRoutes);
