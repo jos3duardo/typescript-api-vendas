515a4577421685b9775b46731ba7f78d2f63af16
jos3duardo <jos3duardolopes@gmail.com>
2021-10-07 00:19:58 -0400

    feat: criando rota de upload de imagem para o avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -2,9 +2,15 @@ import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import UserController from '@modules/users/controllers/UserController';
 import isAutenticated from '@shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+import UserAvatarController from '@modules/users/controllers/UserAvatarController';
 
 const usersRoutes = Router();
 const usersController = new UserController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRoutes.get('/', isAutenticated, usersController.index);
 
@@ -18,6 +24,13 @@ usersRoutes.post(
     },
   }),
   usersController.create,
+);
+
+usersRoutes.patch(
+  'avatar',
+  isAutenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
 );
 
 export default usersRoutes;
